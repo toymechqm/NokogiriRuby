467cfb75647eafa60370096c62e63b4bf42d87b5
Josh <toyjtoy@gmail.com>
2015-10-24 16:50:59 -0400

    add news scrape

--- mech.rb
+++ mech.rb
@@ -2,6 +2,13 @@ require 'mechanize'
 
 mechanize = Mechanize.new
 
-page = mechanize.get('http://stackoverflow.com/')
+page = mechanize.get('http://www.bbc.com/news')
+page2 = mechanize.get('https://www.bostonglobe.com/')
+page3 = mechanize.get('http://www.huffingtonpost.com/')
+page4 = mechanize.get('http://www.foxnews.com/')
 
-puts page.title
+
+puts page.at("span.title-link__title-text").text.strip
+puts page2.at("h2.story-title.hed-lead").text.strip
+puts page3.at("div#splash_block a").text.strip
+puts page4.at("h1 a").text.strip
